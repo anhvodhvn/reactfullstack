3ae877ec50c128d52860160567f5de4da45dfb42
Coder <duyanh2005@gmail.com>
2018-04-01 00:28:17 +0700

    clean up

--- app.js
+++ app.js
@@ -1,7 +1,6 @@
 ﻿const http = require('http');
 const server = require('./server');
 
-
 http.createServer(server).listen(server.get('port'), function () {
-    console.log('eShopping is running on port:' + server.get('port'));    
+    console.log('eShop is running on port:' + server.get('port'));    
 });
