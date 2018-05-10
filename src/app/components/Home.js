74714a6229814ccefb4000535854aa268f3ac43f
anhvodhvn <anh.voduy@harveynash.vn>
2018-05-10 21:32:39 +0700

    clean up

--- src/app/components/Home.js
+++ src/app/components/Home.js
@@ -2,7 +2,6 @@ import React from 'react';
 
 export class Home extends React.Component{
     render(){
-        console.log(this.props);
         return (
             <div>
                 <p>In a new Component!</p>
